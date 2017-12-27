0812c668347a8f10a693b0f7f8eb49eb1abfd3af
Stef Wijnberg <mail@stefw.nl>
2017-12-27 14:44:17 +0100

    Pin 4

--- led.py
+++ led.py
@@ -1,17 +1,19 @@
 # importeer de GPIO bibliotheek.
 import RPi.GPIO as GPIO
 
-ledPin = 7
+ledPin = 4
 
- 
 # Zet de pinmode op Broadcom SOC.
 GPIO.setmode(GPIO.BCM)
 
 # Zet waarschuwingen uit.
-GPIO.setwarnings(False)
+# GPIO.setwarnings(False)
  
 # Zet de GPIO pin als uitgang.
 GPIO.setup(ledPin, GPIO.OUT)
  
 # Zet de LED aan.
 GPIO.output(ledPin, 1)
+
+
+sleep(5)
